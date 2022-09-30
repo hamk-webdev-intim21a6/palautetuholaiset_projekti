789a15258e206b9836bfb9c6f4ad062602be47ce
Robin <robin.selenius@student.hamk.fi>
2022-09-30 22:28:01 +0300

    Login, signup ja logout tehty

--- palautetuholaiset_projekti/settings.py
+++ palautetuholaiset_projekti/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'dictionary.apps.DictionaryConfig',
     'django_extensions',
     'django.contrib.admin',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'palautetuholaiset_projekti.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
