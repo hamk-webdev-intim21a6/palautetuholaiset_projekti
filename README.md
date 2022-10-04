67fb0dbb588d983eef77197e13bfd794eff34634
Aleksi Bovellan <aleksi.bovellan@student.hamk.fi>
2022-10-04 20:14:37 +0300

    Restart

--- README.md
+++
@@ -1,3 +0,0 @@
-# Palautetuholaiset-ryhmän Django-projekti
-## HAMK - INTIM21A6 - Web-projekti - Petri Kuittinen
-### Aleksi Bovellan, Satu Pohjonen, Robin Selenius
--- feedback/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'feedback'
-urlpatterns = [
-    path('', views.FeedbackCreateView.as_view(), name='index'),
-]
