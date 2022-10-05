e0505e38116749f7560156889383881d6d7cc203
Aleksi Bovellan <aleksi.bovellan@student.hamk.fi>
2022-10-05 16:38:27 +0300

    Graphic small fixes

--- feedback/models.py
+++ feedback/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# anna palaute
-# aihe (web-kodaamisen perusteet (5 op))
-# arvosana 1-100
-# hyvää
-# huonoa
-# automaattisesti luotu aikaleima
-
 class Movie(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
@@ -15,10 +8,11 @@ class Movie(models.Model):
 
 class Feedback(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=50, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    rating = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
     good = models.TextField(max_length=2000, blank=True)
     bad = models.TextField(max_length=2000, blank=True) 
     date = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return f"{self.date}"
     
