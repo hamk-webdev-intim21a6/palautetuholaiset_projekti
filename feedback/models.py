dffdc0da4f2d57e30a067c786c118d8e39f41fe7
satupee <satu21019@student.hamk.fi>
2022-10-06 23:17:30 +0300

    Arvosanat valittavaksi alasvetovalikkoon.

--- feedback/models.py
+++ feedback/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Movie(models.Model):
     name = models.CharField(max_length=200)
@@ -12,10 +11,18 @@ class Movie(models.Model):
 
 class Feedback(models.Model):
     elokuva = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    arvosana = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    arvosana_arvo = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5)
+    )
+    arvosana = models.IntegerField(choices = arvosana_arvo, default = 1)
     plussat = models.TextField(max_length=2000, blank=True)
     miinukset = models.TextField(max_length=2000, blank=True) 
     aika = models.DateTimeField(auto_now_add=True)
+ 
     
     def __str__(self):
         return f"{self.aika}"   
