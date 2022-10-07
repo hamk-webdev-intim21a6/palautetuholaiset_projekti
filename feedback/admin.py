81557584ea39064e81d86d15bd9be0385fb2f97a
satupee <satu21019@student.hamk.fi>
2022-10-07 15:04:36 +0300

    Lisätty arvosanojen keskiarvo Movies-sivulle.

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from .models import Movie, Feedback
+from django.db.models import Avg
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'show_average')
     search_fields = ['name']
+
+    def show_average(self, obj):
+        result = Feedback.objects.filter(elokuva=obj).aggregate(Avg("arvosana"))
+        return result["arvosana__avg"]
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('elokuva', 'arvosana', 'plussat', 'miinukset', 'aika')
     list_filter = ['aika', 'elokuva']
     search_fields = ['plussat', 'miinukset']
-   
+
+  
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Feedback, FeedbackAdmin)
 
